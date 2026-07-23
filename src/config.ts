f2d87b1cf15a959535a8f374681b4092807087bd
JulianR10 <julianrecarte@gmail.com>
2026-07-23 19:08:16 +0200

    fix: centrado mobile en Tecnicas, labels stats uniformes, links WhatsApp e Instagram actualizados

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONTACT = {
-  whatsapp: '5491112345678',
-  instagram: 'https://www.instagram.com/',
+  whatsapp: '5493572541856',
+  instagram: 'https://www.instagram.com/estampados.sp/',
   facebook: 'https://www.facebook.com/',
 } as const;
 
