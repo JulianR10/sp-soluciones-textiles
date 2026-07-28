087ac9c13698e4fb43ad3d0bff5ae850d90c603a
JulianR10 <julianrecarte@gmail.com>
2026-07-28 18:35:58 +0200

    fix: base condicional para Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JulianR10.github.io',
-  base: '/sp-soluciones-textiles',
+  base: process.env.VERCEL ? '/' : '/sp-soluciones-textiles',
   devToolbar: {
     enabled: false,
   },
